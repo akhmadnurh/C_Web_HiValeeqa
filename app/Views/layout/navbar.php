31355929388035023ebb43550ab80ce8aa4b4080
dimpangg <72178046+dimpangg@users.noreply.github.com>
2021-05-31 21:10:06 +0700

    Asset & Home

---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,17 @@
+<!-- Start Top Nav -->
+<nav class="navbar navbar-expand-lg bg-dark navbar-light d-none d-lg-block" id="templatemo_nav_top">
+    <div class="container text-light">
+        <div class="w-100 d-flex justify-content-between">
+            <div>
+                <i class="fa fa-envelope mx-2"></i>
+                <a class="navbar-sm-brand text-light text-decoration-none" href="mailto:hi-valeeqa@gmail.com">hi-valeeqa@gmail.com</a>
+                <i class="bi-whatsapp mx-2"></i>
+                <a class="navbar-sm-brand text-light text-decoration-none" href="https://wa.me/6285784197425" target="_blank">+6285784197425</a>
+            </div>
+            <div>
+                <a class="text-light" href="https://www.instagram.com/hi.valeeqa/" target="_blank"><i class="fab fa-instagram fa-sm fa-fw me-1"></i> hi.valeeqa</a>
+            </div>
+        </div>
+    </div>
+</nav>
+<!-- Close Top Nav -->
