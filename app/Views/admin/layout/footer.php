8522695d02c7c47e5978af76d9171078a2edb1c0
Akhmad Nur Hidayatulloh <e41191558@student.polije.ac.id>
2021-06-09 19:33:37 +0700

    Laravel upload

--- app/Views/admin/layout/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <div class="footer-left">
-        Copyright &copy; <?= Date('Y') ?> <div class="bullet"></div> Hi Valeeqa
-    </div>
-    <div class="footer-right">
-        2.3.0
-    </div>
-</footer>
