91981b6efad02a60639ef2ecbf83973950466754
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-06-14 01:31:53 +0700

    Admin sidebar active

--- public/admin/assets/js/custom.js
+++ public/admin/assets/js/custom.js
@@ -6,3 +6,11 @@
  */
 
 "use strict";
+const liSidebar = document.querySelectorAll('ul.sidebar-menu li:not(.menu-header)');
+const linkPage = window.location.href;
+const liLink = document.querySelectorAll('ul.sidebar-menu li a');
+for (let i = 0; i < liLink.length; i++) {
+    if (liLink[i] == linkPage) {
+        liSidebar[i].setAttribute('class', 'active');
+    }
+}
