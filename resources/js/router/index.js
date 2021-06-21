0e704e35a10ce44ac906c3300c52054ecfe82038
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-06-22 01:57:33 +0700

    Login migrate vue

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,6 +1,8 @@
 import Home from '../views/Home'
 import Register from '../views/Register'
 import Shop from '../views/Shop'
+import Login from '../views/Login'
+import Detail from '../views/Detail'
 
 export default {
     mode: 'history',
@@ -12,6 +14,11 @@ export default {
             component: Home
         },
         {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
             path: '/register',
             name: 'user.register',
             component: Register
@@ -20,6 +27,14 @@ export default {
             path: '/shop',
             name: 'user.shop',
             component: Shop
+        },
+        {
+            path: '/detail',
+            name: 'user.detail',
+            component: Detail
         }
-    ]
+    ],
+    scrollBehavior() {
+        document.getElementById('app').scrollIntoView();
+    }
 }
--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-light">
         <div class="container d-flex align-items-center justify-content-center" style="height: 100vh;">
-            <div class="card bg-white border border-white px-5 pt-5 pb-3" style="border-radius: 2rem">
+            <div class="card bg-white border border-white px-5 py-5" style="border-radius: 2rem">
                 <div class="card-body">
                     <div class="row">
                         <div class="d-none d-lg-inline col-lg-7">
@@ -13,35 +13,22 @@
                                     style="height: 1rem">
                                 Hi Valeeqa
                             </h6>
-                            <h2 class="my-4 text-pink">Welcome to Family</h2>
+                            <h2 class="my-4 text-pink">Login</h2>
                             <form action="#" method="POST">
                                 <div class="mb-3">
-                                    <input type="text" class="form-control" id="fullname" name="fullname"
-                                        placeholder="Nama Lengkap">
-                                </div>
-                                <div class="mb-3">
-                                    <input type="email" class="form-control" id="email" name="email"
-                                        aria-describedby="emailHelp"
-                                        placeholder="Email">
-                                </div>
-                                <div class="mb-3">
-                                    <input type="text" class="form-control" id="username" name="username"
-                                        placeholder="Username">
+                                    <input type="text" class="form-control" id="userEmail" name="userEmail"
+                                        placeholder="Username/Email">
                                 </div>
                                 <div class="mb-3">
                                     <input type="password" class="form-control" id="password" name="password"
                                         placeholder="Password">
                                 </div>
-                                <div class="mb-3">
-                                    <input type="password" class="form-control" id="confirmPassword" name="confirmPassword"
-                                        placeholder="Konfirmasi Password">
-                                </div>
                                 <div class="d-flex">
-                                    <button type="submit" class="btn btn-pink btn-lg me-3">Daftar</button>
+                                    <button type="submit" class="btn btn-pink btn-lg me-3">Login</button>
                                     <router-link to="/" class="btn btn-light btn-lg">Back to Home</router-link>
                                 </div>
                             </form>
-                            <p class="text-center mt-5">Sudah punya akun? <router-link class="text-pink" to="/login">Masuk</router-link></p>
+                            <p class="text-center mt-5">Belum punya akun? <router-link class="text-pink" to="/register">Daftar</router-link></p>
                         </div>
                     </div>
                 </div>
@@ -49,3 +36,9 @@
         </div>
     </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
