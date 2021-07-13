3c4efd64869b3362cbc153c7aca58b5be62f11a4
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-07-13 14:33:58 +0700

    user seed

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -14,23 +14,33 @@ class userSeeder extends Seeder
      */
     public function run()
     {
-       DB::table('user')->insert([
-          'name' => 'admin',
-           'email' => 'admin@test.com',
-           'username' => 'admin',
-           'password' => md5('admin'),
-           'gender' => 'L',
-           'address' => 'Mojokerto',
-           'whatsapp' => '088888888888',
-           'role' => '1'
-       ]);
+        DB::table('user')->insert([
+            'name' => 'admin',
+            'email' => 'admin@test.com',
+            'username' => 'admin',
+            'password' => md5('admin'),
+            'gender' => 'L',
+            'province' => 'Jawa Timur',
+            'city' => 'Jember',
+            'district' => 'Sumbersari',
+            'village' => 'Sumbersari',
+            'address' => 'Jl. Mastrip No.164, Krajan Timur',
+            'postal_code' => '68121',
+            'whatsapp' => '088888888888',
+            'role' => '1'
+        ]);
         DB::table('user')->insert([
             'name' => 'user',
             'email' => 'user@test.com',
             'username' => 'user',
             'password' => md5('user'),
             'gender' => 'L',
-            'address' => 'Mojokerto',
+            'province' => 'Jawa Timur',
+            'city' => 'Lumajang',
+            'district' => 'Lumajang',
+            'village' => 'Tompokersan',
+            'address' => 'Jalan Brigjen. Katamso',
+            'postal_code' => '67316',
             'whatsapp' => '088888888888',
             'role' => '0'
         ]);
