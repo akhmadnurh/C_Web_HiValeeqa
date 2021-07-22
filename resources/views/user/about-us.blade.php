a1554b64abbb1a7ce76c5fbd64e378cc7d3d456c
zavira111 <agnesoctavianda123@gmail.com>
2021-07-22 19:47:08 +0700

    tentang kami

---
+++ resources/views/user/about-us.blade.php
@@ -0,0 +1,34 @@
+@extends('user/layout.template')
+
+@section('content')
+
+    <section class="title bg-pink-light">
+        <div class="container p-5">
+            <h2 class="text-center">About Us</h2>
+        </div>
+    </section>
+
+    <section class="main-content my-5">
+        <div class="container align-items-center justify-content-center d-flex flex-column py-5">
+            <div class="card border-radius-1 w-75">
+                <div class="card-body p-lg-5">
+                    <h3 class="card-title mb-4 text-center">HI VALEEQA</h3>
+                    Hi Valeeqa diluncurkan pertama kali pada bulan agustus tahun 2020 sebagai produk untuk memenuhi
+                    kebutuhan fashion muslimah. Hi Valeeqa sendiri menyediakan 2 kategori produk gamis yang setiap produknya
+                    menggunakan material jenis kain yang berbeda tentunya juga mengutamakan kenyamanan penggunaan para
+                    muslimah. Berpacu pada kenyamanan para muslimah, Hi Valeeqa juga tidak luput dari style fashion kekinian
+                    yang mengangkat design simple minimalis dengan warna earth tone yang sifatnya netral dan tidak mencolok
+                    cocok untuk dipadukan dengan khimar yang bermotif atau polos.
+                </div>
+            </div>
+            <h2 class="mt-5">Terhubung Dengan Kami</h2>
+            <a href="https://www.instagram.com/hi.valeeqa"
+               rel="nofollow"
+               target="_blank"
+               class="justify-content-start align-items-center text-pink fs-2 border-radius-1 p-3 mt-4 border border-1 d-flex">
+                <i class='bx bxl-instagram bx-md me-2'></i> hi.valeeqa
+            </a>
+        </div>
+    </section>
+
+@endsection
